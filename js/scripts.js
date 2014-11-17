79133c09bc50cfe695a4a0e5648aef660281fe0c
zjohnson1 <zjohnson@eagles.ewu.edu>
2014-11-17 09:50:35 -0800

    vid

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
-       // $().ready(function() {
-         //       $('#wrapper').tubular({videoId: 'Ljm4Uv0KCSQ'}); // where idOfYourVideo is the YouTube ID.
-        //});
+        $().ready(function() {
+                $('#wrapper').tubular({videoId: 'Ljm4Uv0KCSQ'}); // where idOfYourVideo is the YouTube ID.
+        });
